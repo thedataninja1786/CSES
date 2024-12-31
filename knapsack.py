d7a9cdd01c286f97959fedd07ce03af3bd5c2152
thedataninja1786 <chrissolomou1786@gmail.com>
2024-12-31 21:58:49 +0200

    Added knapsack implementations

---
+++ knapsack.py
@@ -0,0 +1,16 @@
+values = [3, 4, 5]
+weights = [2, 3, 4]
+k = 5
+
+def zero_one(values,weights,capacity):
+    # choose each item at most once
+    dp = [[0 for _ in range(k+1)] for _ in range(len(weights)+1)]
+
+    for i in range(1,len(dp)):
+        for j in range(1,len(dp[0])):
+            if j < weights[i-1]:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i-1][j-weights[i-1]] + values[i-1])
+
+    return dp[-1][-1]
--- two_sets.py
+++ two_sets.py
@@ -1,35 +1,24 @@
-import sys
-sys.setrecursionlimit(10000)
 
-n = 26560
+# find the max value achievable 
+values = [3, 4, 5]
+weights = [2, 3, 4]
+k = 5
+ls = [(v,w) for v,w in zip(values,weights)]
+# expected 7
 
-def two_sets(n:int) -> str:
-    global res
-    nums = [i+1 for i in range(n)]
-    res = []
+"""
+20 12 6
+4  3  2
 
-    def dfs(i,ls,ds):
-        global res
-        if i == len(nums) and sum(ls) == sum(ds):
-            res = [ds,ls]
-            return
-        if i >= len(nums):
-            return 
-        
-        dfs(i+1,ls + [nums[i]],ds)
-        dfs(i+1,ls, ds + [nums[i]])
-        
+"""
+dp = [[0 for _ in range(k+1)] for _ in range(len(weights)+1)]
 
-    dfs(0,[],[])
-    
-    res = sorted(res, key =lambda x:len(x),reverse=True)
-    
-    if not res:
-        return "NO"
-    else:
-        s = "YES"
-        for ls in res:
-            s += "\n" + str(len(ls)) + "\n" + " ".join([str(x) for x in ls])
-        return s
-    
-print(two_sets(n))
+for i in range(1,len(dp)):
+    for j in range(1,len(dp[0])):
+        if j < weights[i-1]:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j],dp[i-1][j-weights[i-1]] + values[i-1])
+
+for l in dp:
+    print(l)
