5fc73430eb9f01733d367da87bfcc110ac7a74f4
thedataninja1786 <chrissolomou1786@gmail.com>
2025-01-01 17:59:32 +0200

    Solution to trailing zeroes

--- trailing_zeroes.py
+++ trailing_zeroes.py
@@ -1,3 +1,20 @@
-from math import factorial 
 
-print(factorial(200000))
+n = int(input())
+
+def trailing_zeroes(n):
+    """ Legendre's formula 
+        Trailing zeroes are caused by 10, which is the product of 
+        2 and 5. Devide by either factor until the denominator 5**p > n
+    """
+    res = 0
+    i = 1
+    while True:
+        if n >= (5 ** i):
+            res += n // 5**i
+        else:
+            return res
+        
+        i += 1
+    
+
+print(trailing_zeroes(n))
