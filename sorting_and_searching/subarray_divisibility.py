daed54924ac61243d7e2dfefd2d5223ac99a7a7d
thedataninja1786 <chrissolomou1786@gmail.com>
2025-01-19 15:22:54 +0200

    Check if the mod of prefix sum has been seen before

--- sorting_and_searching/subarray_divisibility.py
+++ sorting_and_searching/subarray_divisibility.py
@@ -1,39 +1,30 @@
-import sys 
-from collections import defaultdict 
+import sys
+from collections import defaultdict
+data = sys.stdin.read()
 
-data = sys.stdin.read() 
+data = data.split("\n")
+t = int(data[0].split()[-1])
+nums = [int(x) for x in data[1].split()]
 
-def subarray_divisibility(data):
-    # idea:
-    # 1. convert to base t x % t 
-    # 2. prefix sum and convert to base t again
-    # 3. if the number appears again, 
-    #    means divisible by t since x2 - x1 == 0
+"""
+For each prefix sum, calculate its modulo 𝑛
+n. If two prefix sums have the same remainder when divided by 𝑛
+n, the subarray between those two indices is divisible by 𝑛.
+"""
+
+s = cnt = 0 
+c= defaultdict(int)
+c[0] = 1
+for x in nums:
+    s += x
+
+    # This guarantees that the remainder will be in the expected 
+    # range of [0, t-1], avoiding issues with negative values.
+    r = (s % t + t) % t
+
+    if r in c:
+        cnt += c[r]
     
-    
-    data = data.split("\n")
-    t = int(data[0])
-    nums = [int(x) for x in data[1].split()]
-    cnt = 0 
-    c = defaultdict(int)
-    
-    c[0] = 1
-    s = 0
-    nums = [x%t for x in nums]
-    dp = []
+    c[r] += 1
 
-    for x in nums:
-        s += x
-        dp.append(s)
-    
-    nums = [x%t for x in dp]
-    for i in range(len(nums)):
-
-        if nums[i] in c: 
-            cnt += c[nums[i]]
-
-        c[nums[i]] += 1
-
-    return cnt 
-    
-print(subarray_divisibility(data))
+print(cnt)
